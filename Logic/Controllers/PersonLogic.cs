74b7628485d4f3b04a1860100b169c8884177547
unknown <pgramajo@PGRAMAJO-PC.jbknowledge.com>
2016-03-29 11:22:14 -0300

    api rest ember primer commit

---
+++ Logic/Controllers/PersonLogic.cs
@@ -0,0 +1,24 @@
+﻿using Logic.DBConnection;
+using Logic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Controllers
+{
+    public abstract class PersonLogic
+    {
+        public static dynamic GetAllPerson()
+        {
+            using (var db = new EmberContext())
+            {
+                var widgets = db.Database.SqlQuery<Widget>("usp_SmartInsight_Widget_GetAll").ToList();
+
+                return new { widgets = widgets };
+            }
+        } 
+
+    }
+}
---
+++ Logic/Model/Widget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Model
+{
+    public class Widget
+    {
+        public int id { get; set; }
+        public int user { get; set; }
+        public string target { get; set; }
+        public string btnId { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public int height { get; set; }
+        public int width { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public bool selected { get; set; }
+    }
+}
