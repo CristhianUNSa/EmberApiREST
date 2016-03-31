4e1794a67a86c9a469e165dfd83987acdf7fafbc
unknown <pgramajo@PGRAMAJO-PC.jbknowledge.com>
2016-03-31 16:53:48 -0300

    modificaciones para devolver preguntas con sus posibles respuestas

--- Logic/Controllers/PersonLogic.cs
+++ Logic/Controllers/PersonLogic.cs
@@ -11,14 +11,79 @@ namespace Logic.Controllers
     public abstract class PersonLogic
     {
         public static dynamic GetAllPerson()
-        {
-           // return ['uno]
-            using (var db = new EmberContext())
-            {
-                var widgets = db.Database.SqlQuery<Widget>("usp_SmartInsight_Widget_GetAll").ToList();
+        {            
+            Answer oanswer1 = new Answer();
+            oanswer1.id = 1;
+            oanswer1.text = "Licenciado en Sistemas";
+            Answer oanswer2 = new Answer();
+            oanswer2.id = 2;
+            oanswer2.text = "Analista en Sistemas";
+            Answer oanswer3 = new Answer();
+            oanswer3.id = 3;
+            oanswer3.text = "Ingeniero en Informatica";
+            Answer oanswer4 = new Answer();
+            oanswer4.id = 4;
+            oanswer4.text = "Analista Programador";
+            Answer oanswer5 = new Answer();
+            oanswer5.id = 5;
+            oanswer5.text = "Otro";
 
-                return new { people = widgets };
-            }
+            Answer a1 = new Answer();
+            a1.id = 6;
+            a1.text = "Simecom";
+            Answer a2 = new Answer();
+            a2.id = 7;
+            a2.text = "Tel Net";
+            Answer a3 = new Answer();
+            a3.id = 8;
+            a3.text = "YAPP";
+
+
+            Question q1 = new Question();
+            q1.id = 1;
+            q1.text = "Cual es tu Nombre";
+            q1.type="input";
+            q1.answers = null;
+
+            Question q2 = new Question();
+            q2.id = 2;
+            q2.text = "Cual es tu Apellido";
+            q2.type = "input";
+            q2.answers = null;
+            Question q3 = new Question();
+            q3.id = 3;
+            q3.text = "Cual es tu direccion";
+            q3.type = "input";
+            q3.answers = null;
+
+             var personal = new List<Answer> { oanswer1, oanswer2, oanswer3, oanswer4, oanswer5 };
+
+            Question q4 = new Question();
+            q4.id = 4;
+            q4.text = "Cual es tu profesion";
+            q4.type = "checkbox";
+            q4.answers = personal;
+
+            var laboral = new List<Answer> { a1, a2, a3, oanswer5 };
+
+            Question q5 = new Question();
+            q5.id = 5;
+            q5.text = "En que empresa trabajas";
+            q5.type = "checkbox";
+            q5.answers = laboral;
+
+
+            var questions = new List<Question>{q1,q2,q3,q4,q5};
+
+            return new {questions=questions};
+
+
+            //using (var db = new EmberContext())
+            //{
+            //    //var widgets = db.Database.SqlQuery<Widget>("usp_SmartInsight_Widget_GetAll").ToList();
+
+            //   // return new { people = widgets };                
+            //}
         } 
 
     }
